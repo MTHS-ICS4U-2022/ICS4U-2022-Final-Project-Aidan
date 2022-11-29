73e0dca8e508477667d35f8cad71064ef6877b9e
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-11-29 14:45:19 +0000

    hello world!

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Aidan Lalonde-Novales All rights reserved
+//
+// Created by Aidan Lalonde-Novales on 2022-11-29
+// This program prints "Hello World!"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, World!\n";
+  return 0;
+}
